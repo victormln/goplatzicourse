840e0cb3ed0ce9da5efd9e2646ec11e41291504a
victormln <victormln@users.noreply.github.com>
2019-09-01 00:17:18 +0200

    Adding interface

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// Course struct
+type Course struct {
+	Name   string
+	Slug   string
+	Skills []string
+}
+
+// Subscribe method
+func (p Course) Subscribe(name string) {
+	fmt.Printf("The person %s has subscribed to %s course\n", name, p.Name)
+}
+
+// Career struct
+type Career struct {
+	Course
+}
+
+// Subscribe method
+func (p Career) Subscribe(name string) {
+	fmt.Printf("The person %s has subscribed to %s career\n", name, p.Name)
+}
+
+// Organization interface
+type Organization interface {
+	Subscribe(name string)
+}
+
+func callSubscribe(p Organization) {
+	p.Subscribe("Victor")
+}
+
+// InterfaceTest method
+func InterfaceTest() {
+	myCourse := Course{Name: "Go", Slug: "go", Skills: []string{"1", "2"}}
+	myCareer := new(Career)
+	myCareer.Name = "Go 2"
+	myCareer.Slug = "go2"
+	myCareer.Skills = []string{"3", "4"}
+	callSubscribe(myCourse)
+	callSubscribe(myCareer)
+}
+
+func main() {
+	InterfaceTest()
+}
